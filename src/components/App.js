55251a1e79e476430e0c7c5a906096c919bb67ed
shananasim16 <shanashabnam02@gmail.com>
2024-06-25 21:51:44 +0530

    new changes in local

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,13 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [name, setName] = useState('');
-  const handleNameChange = (event) => {
-    setNmae(event.target.value);
-  };
+
+  const [name, setName] = useState("");
   return (
     <div>
-         <h2>Enter your name:</h2>
-    <label>
-        <input>
-           type="text"
-           value={name}
-             onChange={handleNameChange}
-               placeholder="Enter your name"
-                 />
-                 </label>
-  {name && (
-    <p>Hello {name} !</p>
-    )}
+        <p>Enter your name:</p>
+        <input onChange={(e) => setName(e.target.value)}></input>
+        <p>{name}</p>
     </div>
   )
 }
